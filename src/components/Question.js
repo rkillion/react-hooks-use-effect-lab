e6c0dd967c9aa0db82086a0574361cc1ef71e9e3
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-17 12:45:11 -0400

    commit tree d0c0d9ec4e241b00132fc12980879e348d8b5975

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    const timer = setTimeout(()=>{
+      console.log(`Timer running for question ${question.id}.`)
+      if(timeRemaining>0) {
+        setTimeRemaining(timeRemaining=>timeRemaining-1);
+      } else {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    },1000)
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
